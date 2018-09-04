28e07b38580cef9e1878a1a4ec1580511711f52a
huanglinrong <964157675@qq.com>
2018-09-04 22:57:47 +0800

    update urls

--- DjangoTest/DjangoTest/urls.py
+++ DjangoTest/DjangoTest/urls.py
@@ -14,13 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from test1 import views as list_view
-
+import test1.urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',list_view.home_page,name='home'),	
-    path('lists/<int:list_id>/',list_view.view_list,name='view_list'),
-    path('lists/new',list_view.new_list,name='new_list'),
-    path('lists/<int:list_id>/add_item',list_view.add_item,name='add_item'),
+    path('',list_view.home_page,name='home'),
+    path('lists/', include(test1.urls)),	
 ]
