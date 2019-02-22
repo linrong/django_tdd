0905e41b9c9e9513e9c16ab75f115fcb2ba375a2
linrong1994 <964157675@qq.com>
2019-02-22 22:12:37 +0800

    Feat(validation):add test actions

--- DjangoTest/function_tests/test_list_item_validation.py
+++ DjangoTest/function_tests/test_list_item_validation.py
@@ -2,6 +2,22 @@ from .base import FunctionalTest
 from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
-    @skip
     def test_cannot_add_empty_list_items(self):
-        pass
+        self.browser.get(self.server_url) # 访问网页
+        self.browser.find_element_by_id('id_new_item').send_keys('\n') # 按下回车键
+
+        error=self.browser.find_element_by_css_selector('.has-error')# 通过bootstrap提供的css类.has-error标记错误文本
+        self.assertEqual(error.text,"You can't have an empty list item")# 判断错误信息是否和预先的一致
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')# 输入文字并且按下回车
+        self.check_for_row_in_list_table('1: Buy milk')
+
+        self.browser.find_element_by_id('id_new_item').send_keys('\n') # 不输入文字按下回车键
+         
+        self.check_for_row_in_list_table('1: Buy milk')# 再一次检查lsit文字
+        error=self.browser.find_element_by_css_selector('.has-error')# 通过bootstrap提供的css类.has-error标记错误文本
+        self.assertEqual(error.text,"You can't have an empty list item")# 判断错误信息是否和预先的一致
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')# 输入文字并且按下回车
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('1: Make tea')
