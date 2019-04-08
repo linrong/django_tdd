64a5b4e1f76f2f2d89a1078099662829d5fda53a
linrong1994 <964157675@qq.com>
2019-04-08 21:52:17 +0800

    Refactor(单元测试)：重构单元测试，分成views和models测试文件

--- DjangoTest/test1/tests/test_views.py
+++ DjangoTest/test1/tests/test_views.py
@@ -18,36 +18,6 @@ class SmokeTest(TestCase):
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do Lists</title>',response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
-                                
-      
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_=List()
-        list_.save()
-    
-        first_item=Item()
-        first_item.text='first item'
-        first_item.list=list_
-        first_item.save()
-        
-        second_item=Item()
-        second_item.text='second item'
-        second_item.list=list_
-        second_item.save()
-        
-        saved_list=List.objects.first()
-        self.assertEqual(saved_list,list_)
-        
-        saved_items=Item.objects.all()
-        self.assertEqual(saved_items.count(),2)
-        
-        first_saved_item=saved_items[0]
-        second_saved_item=saved_items[1]
-        self.assertEqual(first_saved_item.text,'first item')
-        self.assertEqual(first_saved_item.list,list_)
-        self.assertEqual(second_saved_item.text,'second item')
-        self.assertEqual(second_saved_item.list,list_)
         
 class ListViewTest(TestCase):
     def test_displays_only_items_for_that_list(self):
