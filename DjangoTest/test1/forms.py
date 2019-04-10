0536c9df1f9261d82e21fcdb99f04a9546188d15
linrong1994 <964157675@qq.com>
2019-04-10 22:53:16 +0800

    Feat(forms):替换表单验证失败的默认错误提示，编写单元测试进行验证

--- DjangoTest/test1/forms.py
+++ DjangoTest/test1/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from test1.models import Item
 # 创建表单类
+EMPTY_LIST_ERROR="You can't have an empty list item"
 class ItemForm(forms.models.ModelForm):
     class Meta:
         # 指明表单用于那个模型，使用那些字段
@@ -11,4 +12,7 @@ class ItemForm(forms.models.ModelForm):
                 'placeholder':'Enter a to-do item',
                 'class':'form-control input-lg',
             })
+        }
+        error_messages={
+            'text':{'required':EMPTY_LIST_ERROR}
         }
--- DjangoTest/test1/tests/test_forms.py
+++ DjangoTest/test1/tests/test_forms.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
-from test1.forms import ItemForm
-
+from test1.forms import ItemForm,EMPTY_LIST_ERROR
+from unittest import skip
 class ItemFormTest(TestCase):
+    @skip
     def  test_form_renders_item_text_input(self):
         form=ItemForm()
         self.fail(form.as_p())# form.as_p()作用是把表单渲染成html
@@ -11,3 +12,11 @@ class ItemFormTest(TestCase):
         form=ItemForm()
         self.assertIn('placeholder="Enter a to-do item"',form.as_p())
         self.assertIn('class="form-control input-lg"',form.as_p())
+    
+    def test_form_validation_for_blank_items(self):
+        form=ItemForm(data={'text':''})
+        self.assertFalse(form.is_valid())# form.isvalid()返回验证True或者Flase,如果错误则生成error属性，是字典类型
+        self.assertEqual(
+            form.errors['text'],
+            [EMPTY_LIST_ERROR]
+        )
