5e420c398a99860ff257d03cd6f354d019dba874
linrong1994 <964157675@qq.com>
2019-04-10 22:38:54 +0800

    Refactor(forms):使用ModelForm类自动生成模型的表单

--- DjangoTest/test1/forms.py
+++ DjangoTest/test1/forms.py
@@ -1,9 +1,14 @@
 from django import forms
+from test1.models import Item
 # 创建表单类
-class ItemForm(forms.Form):
-    item_text=forms.CharField(
-        widget=forms.fields.TextInput(attrs={
-            'placeholder':'Enter a to-do item',
-            'class':'form-control input-lg',
-        })
-    )
+class ItemForm(forms.models.ModelForm):
+    class Meta:
+        # 指明表单用于那个模型，使用那些字段
+        model=Item
+        fields=('text',)
+        widgets={
+            'text':forms.fields.TextInput(attrs={
+                'placeholder':'Enter a to-do item',
+                'class':'form-control input-lg',
+            })
+        }
