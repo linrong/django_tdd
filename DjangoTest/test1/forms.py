30f664acc77c59f3fca333c4f1cff8766f2ecdb4
linrong1994 <964157675@qq.com>
2019-04-20 16:50:59 +0800

    Refactor(form):修复功能测试的错误，简化单元测试方法，在其他视图使用表单类，修复之前遗漏的问题，修复可能浏览器更新导致测试不通过问题

--- DjangoTest/test1/forms.py
+++ DjangoTest/test1/forms.py
@@ -11,7 +11,7 @@ class ItemForm(forms.models.ModelForm):
             'text':forms.fields.TextInput(attrs={
                 'placeholder':'Enter a to-do item',
                 'class':'form-control input-lg',
-            })
+            }),
         }
         error_messages={
             'text':{'required':EMPTY_LIST_ERROR}
